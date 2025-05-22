478f7cf43ffb9f196f2cbd893e2ca7eb20044d7a
Neisha2439 <neisha2439@gmail.com>
2025-05-22 15:38:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Selamat datang di web informatika")
 st.write(
     "ngoding seru bersama guru"
 )
-st.image("785f80de3946f807694c531906309c15 (1).jpg ", width=200) 
+st.image("785f80de3946f807694c531906309c15 (1).jpg") 
